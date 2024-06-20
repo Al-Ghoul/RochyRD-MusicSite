7805a0a6518ec713a7a53985e50046c4fea69cd8
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-06-20 07:23:27 +0300

    chore(remove alex): remove alex since he actually did nothing

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -2,14 +2,7 @@ function Footer() {
   return (
     <footer className="flex flex-col gap-1  backdrop-blur-3xl pt-3 rounded-t-xl text-white">
       <p className="text-center text-black">
-        By{" "}
-        <a
-          href="https://www.facebook.com/alexander.ssn1"
-          className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
-        >
-          Alexander Fernández
-        </a>{" "}
-        & AlGhoul
+        By AlGhoul
       </p>
 
       <p className="text-center text-black">
